1d6680a250ca5de190e7fdfd70613bba8488d73a
davidjayjackson <davidjayjackson@gmail.como>
2021-09-30 21:37:08 -0400

    Worked on new plots. 2021-09-30

--- sandbox.R
+++ sandbox.R
@@ -1,49 +1,24 @@
-
-library(dplyr)
-library(tidyr)
-library(ggplot2)
+library(tidyverse)
 library(scales)
 library(dlookr)
 library(stringr)
+library(ggmosaic)
 options(scipen=1)
 rm(list=ls())
 
-# data <- c(40,25,19,37,39)
-# chisq.test(data)
+df <- df %>% filter(race !="animal")
+bkctf <- table(df$race,df$bkctf)
+bkctf <- as.data.frame(bkctf)
 
-df <- readxl::read_xlsx("./LibraOfficeAudioBookDataFinal.xlsx")
-df <- janitor::clean_names(df) # Use Janitor to clean col names
-
-
-##
-TAB <- table(df$race)
-chisq.test(TAB,correct = TRUE)
-race_df<- as.data.frame(TAB)
-
-##
-PT <- prop.table(TAB)
-chisq.test(PT)
-##
-TAB1 <- table(df$race,df$bkcta)
-chisq.test(TAB1,correct = TRUE)
-branch_df <- as.data.frame(TAB1)
-#
-PT <- prop.table(TAB1)
-chisq.test(PT)
-
-##
-df1 <- df %>% 
-  mutate(
-    Race = if_else(str_detect(race, "n/a"), NA_character_, race)
-  )
-
-A <- df %>% select(title,race,bkct_a) 
-books <- head(A,50)
-write.csv(books,file="books.csv",row.names = F)
+bkctf <- bkctf %>%rename(race = Var1)
+bkctf <- bkctf %>%rename(purchased = Var2)
 
 
-table(df$bkcta)
-branch2 <- df %>% select(bkcta)
 
-### Source: https://youtu.be/7vmdHIG9NR8 
-prop.test(168,195,p=0.04,alternative = "two.sided",conf.level = 0.9,correct = FALSE)
+ggplot(bkctf) + geom_mosaic(aes(x=purchased))
+names(bkctf)
+
+data(titanic)
+
+ggplot(bkctf) +
+  geom_mosaic(aes(x = product(race,purchased), fill = Freq))
